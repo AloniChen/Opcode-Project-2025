4895e07c42a57b08e229a699d40dd14f55fb5b2f
Adi Pedel <adipedel@gmail.com>
2025-07-11 16:39:25 +0300

    feature/3.adi.1

--- dispatch_system.py
+++ dispatch_system.py
@@ -1,28 +1,33 @@
 import json
+from classes import Order
+
 def find_order_by_package_id(package_id):
     try:
         with open("orders.json", "r") as file:
             orders = json.load(file)
         for order in orders:
             if order.get("package_id") == package_id:
-                print("ORDER FOUND:")
-                print(f"Package ID: {order['package_id']}")
-                print(f"Customer: {order['customer_id']}")
-                print(f"Courier: {order['courier_id']}")
-                print(f"Route: {order['origin']} → {order['destination']}")
-                print(f"Status: {order['status']}")
-                return
+                this_order = Order(order.get("customer_id"),order.get("courier_id"),order.get("origin_id"),
+                                   order.get('destination_id'),order.get("package_id"),order.get("status"),auto_save=False)
+                return this_order
         print(f"Order {package_id} not found")
+        return None
     except FileNotFoundError:
         print("Orders file not found")
+        return None
 
 def view_orders():
     try:
         with open("orders.json", "r") as file:
             orders = json.load(file)
+        orders_list = []
         for order in orders:
-                print(f"Package ID: {order['package_id']}, Customer: {order['customer_id']}, "
-                      f"Courier: {order['courier_id']}, Route: {order['origin']} → {order['destination']}, "
-                      f"tatus: {order['status']}")
+            orders_list.append(Order(order.get("customer_id"), order.get("courier_id"), order.get("origin_id"),
+                               order.get('destination_id'), order.get("package_id"), order.get("status"),auto_save=False))
+        return orders_list
     except FileNotFoundError:
         print("Orders file not found")
+        return []
+
+def update_order_status(package_id,package_status):
+    Order.update_by_package_id(package_id, "status", package_status)
