7e14d0df4acad6d19765f5ec9397b8450ca0465f
Adi Pedel <adipedel@gmail.com>
2025-07-15 19:45:08 +0300

    feature/3.adi.1

--- dispatch_system.py
+++ dispatch_system.py
@@ -1,33 +1,42 @@
 import json
+from typing import List, Dict
 from classes import Order
 
-def find_order_by_package_id(package_id):
-    try:
-        with open("orders.json", "r") as file:
-            orders = json.load(file)
-        for order in orders:
-            if order.get("package_id") == package_id:
-                this_order = Order(order.get("customer_id"),order.get("courier_id"),order.get("origin_id"),
-                                   order.get('destination_id'),order.get("package_id"),order.get("status"),auto_save=False)
-                return this_order
-        print(f"Order {package_id} not found")
-        return None
-    except FileNotFoundError:
-        print("Orders file not found")
-        return None
+_logger = logging.getLogger(__name__)
 
-def view_orders():
-    try:
-        with open("orders.json", "r") as file:
-            orders = json.load(file)
-        orders_list = []
-        for order in orders:
-            orders_list.append(Order(order.get("customer_id"), order.get("courier_id"), order.get("origin_id"),
-                               order.get('destination_id'), order.get("package_id"), order.get("status"),auto_save=False))
-        return orders_list
-    except FileNotFoundError:
-        print("Orders file not found")
-        return []
 
-def update_order_status(package_id,package_status):
-    Order.update_by_package_id(package_id, "status", package_status)
+class DispatchSystem:
+    """
+    A class to manage the dispatch system.
+    """
+
+    def update_order_status(self, package_id, package_status) -> None:
+        Order.update_by_package_id(package_id, "status", package_status)
+
+    def view_orders(self) -> List[Order]:
+        try:
+            with open("orders.json", "r") as file:
+                orders = json.load(file)
+            orders_list = []
+            for order in orders:
+                orders_list.append(Order(order.get("customer_id"), order.get("courier_id"), order.get("origin_id"),
+                                         order.get('destination_id'), order.get("package_id"), order.get("status"), auto_save=False))
+            return orders_list
+        except FileNotFoundError:
+            print("Orders file not found")
+            return []
+
+    def find_order_by_package_id(self, package_id) -> Order:
+        try:
+            with open("orders.json", "r") as file:
+                orders = json.load(file)
+            for order in orders:
+                if order.get("package_id") == package_id:
+                    this_order = Order(order.get("customer_id"), order.get("courier_id"), order.get("origin_id"),
+                                       order.get('destination_id'), order.get("package_id"), order.get("status"), auto_save=False)
+                    return this_order
+            print(f"Order {package_id} not found")
+            return None
+        except FileNotFoundError:
+            print("Orders file not found")
+            return None
